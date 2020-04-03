98f69a104b0b3ac398b33e57c8f8a26d9f766f45
Joerme <1210856886@qq.com>
2020-04-03 18:48:54 +0800

    读取留言界面

--- app/src/main/java/cn/edu/hdu/artalk2/ReadActivity.java
+++ app/src/main/java/cn/edu/hdu/artalk2/ReadActivity.java
@@ -1,13 +1,28 @@
 package cn.edu.hdu.artalk2;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ReadActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+import cn.edu.hdu.artalk2.adapter.ReadAdapter;
+
+public class ReadActivity extends AppCompatActivity {
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
+
+        viewPager = findViewById(R.id.readviewpager);
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(new ReadMessageFragment());
+        fragments.add(new ReadCommentFragment());
+        fragments.add(new ReadCommentFragment());
+        ReadAdapter adapter = new ReadAdapter(fragments,getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/cn/edu/hdu/artalk2/adapter/ReadAdapter.java
@@ -0,0 +1,28 @@
+package cn.edu.hdu.artalk2.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class ReadAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragments;
+    public ReadAdapter(List<Fragment> fragments, FragmentManager fm){
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+}
