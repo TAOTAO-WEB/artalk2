7c79b87f627fb391b9f327227694fc935a9c3bd8
WiGig11 <chzwyyx9648@163.com>
2020-04-04 20:21:19 +0800

    add number1

--- app/src/main/java/cn/edu/hdu/artalk2/LeaveMessageActivity.java
+++ app/src/main/java/cn/edu/hdu/artalk2/LeaveMessageActivity.java
@@ -1,13 +1,169 @@
 package cn.edu.hdu.artalk2;
 
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 public class LeaveMessageActivity extends AppCompatActivity {
+
+    private static final String TAG ="message" ;
+    private TextView lm_date,lm_time;
+    private ImageView lm_start;
+    private ImageButton lm_mic;
+    private EditText lm_et;
+
+
+    private boolean isStart = false;
+    private MediaRecorder mr = null;
+    private MediaPlayer mp=null;
+    private File dir;
+    private File soundFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);//去标题头
         setContentView(R.layout.activity_leave_message);
+
+
+
+        lm_date = findViewById(R.id.lm_tv1);
+        lm_time=findViewById(R.id.lm_tv3);
+        lm_mic = findViewById(R.id.lm_mic);
+        lm_et = findViewById(R.id.lm_et);
+        lm_start=findViewById(R.id.lm_start);
+
+        //显示时间
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd   HH:mm:ss");
+        Date curDate = new Date(System.currentTimeMillis());
+        lm_date.setText(simpleDateFormat.format(curDate));
+        //缓存声音
+        dir = new File(Environment.getExternalStorageDirectory(),"sounds");
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+        soundFile = new File(dir,System.currentTimeMillis()+".amr");
+        if(!soundFile.exists()){
+            try {
+                soundFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        lm_mic.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        startRecord();
+                        updateMicStatus();
+                        lm_time.setText("松开停止录制");
+                        isStart=true;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        stopRecord();
+                        lm_time.setText("录音完毕");
+                        lm_mic.setImageDrawable(getResources().getDrawable(R.drawable.leavemessage_mic_finitsh));
+                        isStart = false;
+                        lm_start.setVisibility(View.VISIBLE);
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
+        lm_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp=new MediaPlayer();
+                try {
+                    mp.setDataSource(soundFile.getPath());
+                    mp.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mp.start();
+            }
+        });
+
+
+
+    }
+    //开始录制
+    private void startRecord(){
+
+        if(mr == null){
+
+            mr = new MediaRecorder();
+            mr.setAudioSource(MediaRecorder.AudioSource.MIC);  //音频输入源
+            mr.setOutputFormat(MediaRecorder.OutputFormat.AMR_WB);   //设置输出格式
+            mr.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_WB);   //设置编码格式
+            mr.setOutputFile(soundFile.getAbsolutePath());
+            try {
+                mr.prepare();
+                mr.start();  //开始录制
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        else mr.reset();
+    }
+
+
+    private void stopRecord(){
+        if(mr != null){
+            mr.stop();//停止录制
+            mr.release();//资源释放
+            mr = null;
+        }
+    }
+
+    private final Handler mHandler = new Handler();
+    private Runnable mUpdateMicStatusTimer = new Runnable() {
+        public void run() {
+            updateMicStatus();
+        }
+    };
+    /**
+     * 更新话筒状态
+     *
+     */
+    private int BASE = 1;
+    private int SPACE = 100;// 间隔取样时间
+
+    private void updateMicStatus() {
+        if (mr != null) {
+            double ratio = (double)mr.getMaxAmplitude() /BASE;
+            double db = 0;// 分贝
+            if (ratio > 1)
+                db = 20 * Math.log10(ratio);
+            Log.d(TAG,"分贝值："+db);
+            lm_mic.getDrawable().setLevel((int) (3000 + 6000 * db / 100));
+            mHandler.postDelayed(mUpdateMicStatusTimer, SPACE);
+        }
+    }
+    //释放资源
+    private void releaseRecord(){
     }
 }
--- app/src/main/java/cn/edu/hdu/artalk2/MapActivity.java
+++ app/src/main/java/cn/edu/hdu/artalk2/MapActivity.java
@@ -1,14 +1,278 @@
 package cn.edu.hdu.artalk2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.app.ActionBar;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-//主界面
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+import com.google.android.material.internal.*;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapActivity extends AppCompatActivity {
+
+    //地图相关
+    private MapView mMapView = null;
+    private BaiduMap mBaiduMap = null;
+    private Context context;
+    private ImageButton message_button = null;
+    private ImageButton scan_button = null;
+    //定位相关
+    private double mLatitude;
+    private double mLongtitude;
+    //方向传感器
+    private MyOrientationListener mMyOrientationListener;
+    private float mCurrentX;
+    //自定义图标
+    private BitmapDescriptor mIconLocation;
+    private LocationClient mLocationClient;
+    public BDAbstractLocationListener myListener;
+    private LatLng mLastLocationData;
+    private boolean isFirstin = true;
+
+    //底部导航栏
+//    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+//        @Override
+//        //底部导航栏切换界面
+//        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//            switch (menuItem.getItemId()){
+//                case R.id.community:
+//                    Toast.makeText(MapActivity.this,"点击跳转到社区community",Toast.LENGTH_SHORT).show();
+//                    return true;
+//                case R.id.record:
+//                    Toast.makeText(MapActivity.this,"点击跳转到记录record界面",Toast.LENGTH_SHORT).show();
+//                    return true;
+//                case R.id.location:
+//                    Toast.makeText(MapActivity.this,"点击在此定位",Toast.LENGTH_SHORT).show();
+//                    return true;
+//                case R.id.channel:
+//                    Toast.makeText(MapActivity.this,"点击跳转到频道channel界面",Toast.LENGTH_SHORT).show();
+//                    return true;
+//                case R.id.mine:
+//                    Toast.makeText(MapActivity.this,"点击跳转到我的mine界面",Toast.LENGTH_SHORT).show();
+//                    return true;
+//            }
+//            return false;
+//        }
+//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());//初始化SDK
         setContentView(R.layout.activity_map);
+        SDKInitializer.setCoordType(CoordType.BD09LL);//定位编码设置
+        this.context = this;
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        //获取地图控件引用
+        mBaiduMap = mMapView.getMap();//显示地图
+        initMyLocation();//初始化位置方法
+        //两个按钮借口
+        message_button();
+        scan_buttton();
+
+    }
+    //关于地图图层生命周期的五个方法
+    protected void onStart() {
+        super.onStart();
+        //开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        if (!mLocationClient.isStarted())
+            mLocationClient.start();
+        //开启方向传感器
+        mMyOrientationListener.start();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //停止定位
+        mBaiduMap.setMyLocationEnabled(false);
+        mLocationClient.stop();
+        //停止方向传感器
+        mMyOrientationListener.stop();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mBaiduMap.setMyLocationEnabled(false);
+        mMapView.onDestroy();
+        mMapView = null;
+    }
+
+    //按钮响应
+    // 点击信息跳转
+    private void message_button() {
+        //按钮
+        ImageButton message_button = (ImageButton)findViewById(R.id.message_button);
+//        //按钮处理
+        message_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MapActivity.this,"点击跳转到消息界面",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+//点击扫描跳转
+    public void scan_buttton(){
+        ImageButton scan_button = (ImageButton)findViewById(R.id.scan_button);
+        scan_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MapActivity.this,"点击跳转到扫描界面",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    //定位
+    private class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null){
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(mCurrentX).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            //设置自定义图标
+            MyLocationConfiguration config = new
+                    MyLocationConfiguration(
+                    MyLocationConfiguration.LocationMode.NORMAL, true, mIconLocation);
+            mBaiduMap.setMyLocationConfiguration(config);
+            //更新经纬度
+            mLatitude = location.getLatitude();
+            mLongtitude = location.getLongitude();
+            //设置起点
+            mLastLocationData = new LatLng(mLatitude, mLongtitude);
+            if (isFirstin) {
+                centerToMyLocation(location.getLatitude(), location.getLongitude());
+
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {
+                    // GPS定位结果
+                    Toast.makeText(context, "定位:"+location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                    // 网络定位结果
+                    Toast.makeText(context, "定位:"+location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
+                    // 离线定位结果
+                    Toast.makeText(context, "定位:"+location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    Toast.makeText(context, "定位:服务器错误", Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    Toast.makeText(context, "定位:网络错误", Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    Toast.makeText(context, "定位:手机模式错误，请检查是否飞行", Toast.LENGTH_SHORT).show();
+                }
+                isFirstin = false;
+            }
+        }
+    }
+    //初始化定位
+    private void initMyLocation() {
+        //缩放地图
+        MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(15.0f);
+        mBaiduMap.setMapStatus(msu);
+        //开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        //声明LocationClient类
+        mLocationClient = new LocationClient(this);
+        //通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setIsNeedAddress(true);//设置是否需要地址信息
+        option.setScanSpan(1000);
+        //设置locationClientOption
+        mLocationClient.setLocOption(option);
+        myListener = new MyLocationListener();
+        //注册监听函数
+        mLocationClient.registerLocationListener(myListener);
+        //初始化图标
+        mIconLocation = BitmapDescriptorFactory.fromResource(R.mipmap.dituzhiyin);
+        initOrientation();
+        //开始定位
+        mLocationClient.start();
+    }
+    //回到定位中心
+    private void centerToMyLocation(double latitude, double longtitude) {
+        mBaiduMap.clear();
+        mLastLocationData = new LatLng(latitude, longtitude);
+        MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(mLastLocationData);
+        mBaiduMap.animateMapStatus(msu);
+    }
+    //传感器
+    private void initOrientation() {
+        //传感器
+        mMyOrientationListener = new MyOrientationListener(context);
+        mMyOrientationListener.setOnOrientationListener(new MyOrientationListener.OnOrientationListener() {
+            @Override
+            public void onOrientationChanged(float x) {
+                mCurrentX = x;
+            }
+        });
     }
 }
+
+
