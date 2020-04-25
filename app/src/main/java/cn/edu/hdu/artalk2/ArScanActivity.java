ecee575dfc48411eef7e3cb0309b8dfa0fc91f22
bababadboy <sc30logo@gmail.com>
2020-04-26 00:13:50 +0800

    update ar structure

---
+++ app/src/main/java/cn/edu/hdu/artalk2/ArScanActivity.java
@@ -0,0 +1,340 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.edu.hdu.artalk2;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
+import com.google.ar.core.Trackable;
+import com.google.ar.core.TrackingState;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.core.exceptions.UnavailableException;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.HitTestResult;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.ViewRenderable;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import cn.edu.hdu.artalk2.utils.ArUtils;
+
+/**
+ * ar扫描界面
+ */
+public class ArScanActivity extends AppCompatActivity {
+  private static final int RC_PERMISSIONS = 0x123;
+  private boolean cameraPermissionRequested;
+
+  private GestureDetector gestureDetector;
+  private Snackbar loadingMessageSnackbar = null;
+
+  private ArSceneView arSceneView;
+
+  private ViewRenderable audioCardRenderable;
+
+  // True once scene is loaded
+  private boolean hasFinishedLoading = false;
+
+  // True once the scene has been placed.
+  private boolean hasPlacedSolarSystem = false;
+
+  @Override
+  @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
+  // CompletableFuture requires api level 24
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    if (!ArUtils.checkIsSupportedDeviceOrFinish(this)) {
+      // Not a supported device.
+      return;
+    }
+
+    setContentView(R.layout.activity_ar_scan);
+    arSceneView = findViewById(R.id.ar_scene_view);
+
+
+    // Build a renderable from a 2D View.
+    CompletableFuture<ViewRenderable> audioCardStage =
+            ViewRenderable.builder().setView(this, R.layout.audio_card).build();
+    CompletableFuture.allOf(
+            audioCardStage)
+        .handle(
+            (notUsed, throwable) -> {
+              // When you build a Renderable, Sceneform loads its resources in the background while
+              // returning a CompletableFuture. Call handle(), thenAccept(), or check isDone()
+              // before calling get().
+
+              if (throwable != null) {
+                ArUtils.displayError(this, "Unable to load renderable", throwable);
+                return null;
+              }
+
+              try {
+                audioCardRenderable = audioCardStage.get();
+                // Everything finished loading successfully.
+                hasFinishedLoading = true;
+
+              } catch (InterruptedException | ExecutionException ex) {
+                ArUtils.displayError(this, "Unable to load renderable", ex);
+              }
+
+              return null;
+            });
+
+    // Set up a tap gesture detector.
+    gestureDetector =
+        new GestureDetector(
+            this,
+            new GestureDetector.SimpleOnGestureListener() {
+              @Override
+              public boolean onSingleTapUp(MotionEvent e) {
+                onSingleTap(e);
+                return true;
+              }
+
+              @Override
+              public boolean onDown(MotionEvent e) {
+                return true;
+              }
+            });
+
+    // Set a touch listener on the Scene to listen for taps.
+    arSceneView
+        .getScene()
+        .setOnTouchListener(
+            (HitTestResult hitTestResult, MotionEvent event) -> {
+              // If the solar system hasn't been placed yet, detect a tap and then check to see if
+              // the tap occurred on an ARCore plane to place the solar system.
+              if (!hasPlacedSolarSystem) {
+                return gestureDetector.onTouchEvent(event);
+              }
+
+              // Otherwise return false so that the touch event can propagate to the scene.
+              return false;
+            });
+
+    // Set an update listener on the Scene that will hide the loading message once a Plane is
+    // detected.
+    arSceneView
+        .getScene()
+        .addOnUpdateListener(
+            frameTime -> {
+              if (loadingMessageSnackbar == null) {
+                return;
+              }
+
+              Frame frame = arSceneView.getArFrame();
+              if (frame == null) {
+                return;
+              }
+
+              if (frame.getCamera().getTrackingState() != TrackingState.TRACKING) {
+                return;
+              }
+
+              for (Plane plane : frame.getUpdatedTrackables(Plane.class)) {
+                if (plane.getTrackingState() == TrackingState.TRACKING) {
+                  hideLoadingMessage();
+                }
+              }
+            });
+
+    // Lastly request CAMERA permission which is required by ARCore.
+    ArUtils.requestCameraPermission(this, RC_PERMISSIONS);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    if (arSceneView == null) {
+      return;
+    }
+
+    if (arSceneView.getSession() == null) {
+      // If the session wasn't created yet, don't resume rendering.
+      // This can happen if ARCore needs to be updated or permissions are not granted yet.
+      try {
+        Config.LightEstimationMode lightEstimationMode =
+            Config.LightEstimationMode.ENVIRONMENTAL_HDR;
+        Session session =
+            cameraPermissionRequested
+                ? ArUtils.createArSessionWithInstallRequest(this, lightEstimationMode)
+                : ArUtils.createArSessionNoInstallRequest(this, lightEstimationMode);
+        if (session == null) {
+          cameraPermissionRequested = ArUtils.hasCameraPermission(this);
+          return;
+        } else {
+          arSceneView.setupSession(session);
+        }
+      } catch (UnavailableException e) {
+        ArUtils.handleSessionException(this, e);
+      }
+    }
+
+    try {
+      arSceneView.resume();
+    } catch (CameraNotAvailableException ex) {
+      ArUtils.displayError(this, "Unable to get camera", ex);
+      finish();
+      return;
+    }
+
+    if (arSceneView.getSession() != null) {
+      showLoadingMessage();
+    }
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    if (arSceneView != null) {
+      arSceneView.pause();
+    }
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    if (arSceneView != null) {
+      arSceneView.destroy();
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(
+      int requestCode, @NonNull String[] permissions, @NonNull int[] results) {
+    if (!ArUtils.hasCameraPermission(this)) {
+      if (!ArUtils.shouldShowRequestPermissionRationale(this)) {
+        // Permission denied with checking "Do not ask again".
+        ArUtils.launchPermissionSettings(this);
+      } else {
+        Toast.makeText(
+                this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+            .show();
+      }
+      finish();
+    }
+  }
+
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    if (hasFocus) {
+      // Standard Android full-screen functionality.
+      getWindow()
+          .getDecorView()
+          .setSystemUiVisibility(
+              View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_FULLSCREEN
+                  | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+      getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+  }
+
+  private void onSingleTap(MotionEvent tap) {
+    if (!hasFinishedLoading) {
+      // We can't do anything yet.
+      return;
+    }
+
+    Frame frame = arSceneView.getArFrame();
+    if (frame != null) {
+      if (!hasPlacedSolarSystem && tryPlaceSolarSystem(tap, frame)) {
+        hasPlacedSolarSystem = true;
+      }
+    }
+  }
+
+  private boolean tryPlaceSolarSystem(MotionEvent tap, Frame frame) {
+    if (tap != null && frame.getCamera().getTrackingState() == TrackingState.TRACKING) {
+      for (HitResult hit : frame.hitTest(tap)) {
+        Trackable trackable = hit.getTrackable();
+        if (trackable instanceof Plane && ((Plane) trackable).isPoseInPolygon(hit.getHitPose())) {
+          // Create the Anchor.
+          Anchor anchor = hit.createAnchor();
+          AnchorNode anchorNode = new AnchorNode(anchor);
+          anchorNode.setParent(arSceneView.getScene());
+          Node solarSystem = createSolarSystem();
+          anchorNode.addChild(solarSystem);
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  private Node createSolarSystem() {
+    Node base = new Node();
+    Node audioCardNode = new Node();
+    audioCardNode.setParent(base);
+    audioCardNode.setRenderable(audioCardRenderable);
+    audioCardNode.setLocalPosition(new Vector3(0.0f,0.1f,0.0f));
+
+    View audioCardView = audioCardRenderable.getView();
+
+    audioCardView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(ArScanActivity.this,"You touch the card!!",Toast.LENGTH_LONG).show();
+      }
+    });
+    return base;
+  }
+
+
+  private void showLoadingMessage() {
+    if (loadingMessageSnackbar != null && loadingMessageSnackbar.isShownOrQueued()) {
+      return;
+    }
+
+    loadingMessageSnackbar =
+        Snackbar.make(
+            ArScanActivity.this.findViewById(android.R.id.content),
+            R.string.plane_finding,
+            Snackbar.LENGTH_INDEFINITE);
+    loadingMessageSnackbar.getView().setBackgroundColor(0xbf323232);
+    loadingMessageSnackbar.show();
+  }
+
+  private void hideLoadingMessage() {
+    if (loadingMessageSnackbar == null) {
+      return;
+    }
+
+    loadingMessageSnackbar.dismiss();
+    loadingMessageSnackbar = null;
+  }
+}
